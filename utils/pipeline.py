de9407a3c0335439d89f33d7e0e8c7f6005e83b6
Alexandre Nadin <alex.nadin12@gmail.com>
2018-11-08 11:23:37 +0100

    [utils:pipeline] Moved and factorized PipelineManager's configuration related methods in ConfigManagerTemplate class; Overrode ConfigManagerTemplate's LoadConfig() method in PipelineConfigManager and SamplesConfigManager subclasses.

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -16,9 +16,16 @@ class PipelineManager(object):
     self.cleanables = []
     self.log        = Logging("pipe:{}".format(name), "INFO")
     self.samples    = None
-    self.pipeline_confman = PipelineConfigManager('config', self.name) #.autoconfig()
-    self.samples_confman  = SamplesConfigManager('samples', self.name) #.autoconfig()
-    self.loadDftConfigs()
+    self.pipeline_confman = PipelineConfigManager(
+      'config', 
+      self.name,
+      namespace=self.namespace
+    ).loadDftConfig()
+    self.samples_confman = SamplesConfigManager(
+      'samples', 
+      self.name, 
+      namespace=self.namespace
+    ).loadDftConfig() 
  
   @property
   def workflow(self):
@@ -33,48 +40,8 @@ class PipelineManager(object):
       self.home, "pipelines", self.name, "{}.sk".format(self.name)
     )
 
-  def loadDftConfigs(self):
-    self.loadDftPipelineConfig()
-    self.loadDftSamplesConfig()
-
-  def loadDftPipelineConfig(self):
     """
-    Loads the default pipeline configuration file if it exists.
     """
-    conf = self.pipeline_confman.configFileDefault
-    if conf:
-      self.loadConfig(conf)
-
-  def loadConfig(self, file):
-    """
-    Loads the given snakemake configuration file.
-    Updates and converts it to an Addict.
-    """
-    self.workflow.configfile(file)
-    self.updateConfig()
- 
-  def loadDftSamplesConfig(self):
-    """
-    Loads the default pipeline configuration file if it exists.
-    """
-    conf = self.samples_confman.configFileDefault
-    if conf:
-      self.loadSamples(conf)
-  
-  def loadSamples(self, file, indexlowcase_cols=True):
-    """
-    Loads a samples file into a pandas dataframe.
-    If specified, lowercases the column names. 
-    """
-    import pandas as pd
-    filetype = files.extension(file).lstrip('.')
-    fread = getattr(pd, 'read_{}'.format(filetype))
-    data = fread(file)
-    if indexlowcase_cols:
-      data.columns = map(str.lower, data.columns)
-    """ Replace NaN with None """
-    data = data.astype(object).where(pd.notnull(data), None)
-    self.samples = data
 
   @property
   def config(self):
@@ -103,13 +70,6 @@ class PipelineManager(object):
       return self.configFromKeys(config[keys[0]], keys[1:])
     else:
       return config[keys[0]]
-
-  def updateConfig(self):
-    """
-    Converts the config from a regular Python dictionnary into an addict's.
-    """
-    import addict
-    self.namespace['config'] = addict.Dict(self.config)
 
   # ------------ 
   # Snakefiles
@@ -186,13 +146,35 @@ from dataclasses import dataclass
 @dataclass
 class ConfigManagerTemplate(object):
   """ Params """
-  config_type    : str 
-  config_prefix  : str = ""
+  config_type    : str
+  config_prefix  : str  = ""
+  namespace      : dict = None
 
-  """ Attributes """
   extensions = ()
+  """ Attributes """
   def __post_init__(self):
     self.log = Logging("pipe:{}".format(self.config_type), "INFO")
+
+  def loadDftConfig(self):
+    """
+    Loads the default pipeline configuration file if it exists.
+    """
+    config = self.configFileDefault
+    if config:
+      self.loadConfig(config)
+    return self
+
+  def loadConfig(self, file):
+    raise Exception(
+      "Function {} has to be overridden in a subclass."
+       .format(sys._getframe().f_code.co_name))
+
+  def updateNamespaceConfig(self):
+    """
+    Converts the config from a regular Python dictionnary into an addict's.
+    """
+    import addict
+    self.namespace['config'] = addict.Dict(self.namespace['config'])
 
   @property
   def configFileDefault(self):
@@ -255,9 +237,34 @@ class ConfigManagerTemplate(object):
 
 class PipelineConfigManager(ConfigManagerTemplate):
   extensions = ('.yaml', '.json',)
+  def loadConfig(self, file):
+    """
+    Loads the given snakemake configuration file.
+    Updates and converts it to an Addict.
+    """
+    self.namespace['workflow'].configfile(file)
+    self.updateNamespaceConfig()
+    
   
 class SamplesConfigManager(ConfigManagerTemplate):
   extensions = ('.csv', '.tsv',)
+  def loadConfig(self, file, indexlowcase_cols=True):
+    """
+    Loads a samples file into a pandas dataframe.
+    If specified, lowercases the column names. 
+    """
+    import pandas as pd
+    filetype = files.extension(file).lstrip('.')
+    fread = getattr(pd, 'read_{}'.format(filetype))
+    data = fread(file)
+    if indexlowcase_cols:
+      data.columns = map(str.lower, data.columns)
+
+    """ Replace NaN with None """
+    data = data.astype(object).where(pd.notnull(data), None)
+ 
+    """ Update pipeline manager's samples """
+    self.namespace['pipeline_manager'].samples = data
 
 class Pipeline():
   def __init__(self, path):
