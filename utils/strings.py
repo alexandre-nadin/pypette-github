ebf7c5cd7d869a3f0288906b5425b5b4ee642fa2
Alexandre Nadin <alex.nadin12@gmail.com>
2019-01-23 17:20:58 +0100

    [utils:strings:StringFormatter] Changed argument name.

--- utils/strings.py
+++ utils/strings.py
@@ -4,12 +4,14 @@ class StringFormatter(str):
   """
   This class is used to add more flexibility to handling string formats.
   """
-  def formatMapFlexi(self, d, nomissing=True):
+
+  def formatMapFlexi(self, d, nokeyword=True):
     """
     Formats only the string keywords found in the given dictionnary in flexible way.
-    Empties other keywords if nomissing. Else leaves them untouched for further formatting.
+    Empties other keywords if nokeyword. Else leaves them untouched for further formatting.
     """
-    return self.format_map(utils.dicts.Default(d, nomissing))
+    self = StringFormatter(self.format_map(utils.dicts.Default(d, nokeyword)))
+    return self
 
   def keywords(self): 
     """ Returns own format keywords """ 
