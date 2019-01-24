2bc2634a75679b38e8c550abb750b46768e4f127
Alexandre Nadin <alex.nadin12@gmail.com>
2019-01-24 12:11:16 +0100

    [modules:fastq] Moved functions in fastq.py and included it in fastq.sk; Left rules in fastq.sk.

---
+++ modules/fastq/fastq.py
@@ -0,0 +1,67 @@
+def setConfigs(**kwargs):
+  """
+  Not elegant way to deal with config files.
+  Should set dependence to cluster.yaml (retreived in {prj_name}/metadata.json)
+  Loads {prj_name}/metadata.json.
+  """
+  import os
+  """ Load cluster configuration """
+  pipeman.config_manager.loadConfig(
+    os.path.join(
+      pipeman.dir_modules, "lims", "cluster.yaml"
+    )
+  )
+  """ Load project metadata configuration """
+  pipeman.config_manager.loadConfig(
+    "{prj_name}/metadata.json".format(**kwargs)
+  )
+
+def fastq__getRuns():
+  return [ 
+    os.path.join(pipeman.config.cluster.sequencing_runs_dir, runid)
+    for runid in pipeman.config.project.run_ids
+  ]
+
+def fastq__checkRuns(runs=[]):
+  error = False
+  for run in runs:
+    if not os.path.isdir(run):
+      pipeman.log.error("Run {} doesn't exist.".format(run))
+      error = True
+  if error: 
+    raise
+
+def getRunFromFilepath(filepath):
+  """ 
+  Retrieves a Run id from a file containing paths. 
+  """
+  for run in pipeman.config.project.run_ids:
+    run_path = os.path.join(
+      pipeman.config.cluster.sequencing_runs_dir,
+      run, 
+    )
+    if filepath.startswith(run_path):
+      return run
+    else:
+      continue
+
+def mapFastqFilename(filename):
+  """
+  Maps the illumina metadata based on the given filename.
+  """
+  return [ 
+    FastqFile(
+      filename.strip(), 
+      run_name= getRunFromFilepath(filename)
+    ).__dict__[field]
+     for field in list(FastqFile.fieldNames())
+  ]   
+
+def fastq__loadSamples(**kwargs):
+  if pipeman.samples.data is None:
+    pipeman.samples.load(fastq__mapped_samples_io_dft.format(**kwargs))
+
+def mapStringSamples(s, **kwargs):
+  fastq__loadSamples(**kwargs)
+  return pipeman.samples.buildStringFromKeywords(s, **kwargs)
+  
