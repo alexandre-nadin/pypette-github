a6c7074996c2d1933d83d4d8a533b1ec5cf2c4a6
Alexandre Nadin <alex.nadin12@gmail.com>
2019-02-01 13:34:41 +0100

    [modules:multiqc] Finished mapping bamstat rules; Wrote the multiqc rule.

--- modules/mapping/mapping.py
+++ modules/mapping/mapping.py
@@ -1,2 +1,13 @@
-def mapping__aligner():
-  return pipeman.config.pipeline.modules.mapping.keys()[0]
+def mapping__setAligner(name):
+  workflow.globals[mapping__alignerVarName()] = name
+
+def mapping__alignerDft():
+  return 'aligner'
+
+def mapping__alignerVarName():
+  return '_mapping__aligner'
+
+def mapping__getAligner():
+  if not mapping__alignerVarName() in workflow.globals.keys():
+    mapping__setAligner(mapping__alignerDft())
+  return workflow.globals[mapping__alignerVarName()]
