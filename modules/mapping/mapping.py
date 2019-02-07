872bc58ceb969598a5162542eaa91bbc121a2237
Alexandre Nadin <alex.nadin12@gmail.com>
2019-02-07 16:07:21 +0100

    [modules:mapping] Added 'mapping__genomeIndex()' function to retrieve general genome index for any kind of aligner found pipeline configuration; Updated star alignment rule accordingly.

--- modules/mapping/mapping.py
+++ modules/mapping/mapping.py
@@ -11,3 +11,28 @@ def mapping__getAligner():
   if not mapping__alignerVarName() in workflow.globals.keys():
     mapping__setAligner(mapping__alignerDft())
   return workflow.globals[mapping__alignerVarName()]
+
+def mapping__aligner():
+  mappingKeys = list(pipeman.config.pipeline.modules.mapping.keys())
+  if mappingKeys:
+    return mappingKeys[0]
+  else:
+    pipeman.log.error("No aligner found in pipeline configuration.")
+    raise
+
+@cluster__prefixMountPoint
+def mapping__genomeIndex():
+  """
+  Retrieves the genome index using cluster and project metadata parameters.
+  Uses 
+   - cluster.genome_dir
+   - project.genome .gencode and .species
+  """
+  return os.path.join(
+    "{cluster.genome_dir}",
+    "{genome.name}",
+    "{aligner.command}").format(
+      cluster  = pipeman.config.cluster,
+      genome   = pipeman.config.project.genome,
+      aligner  = pipeman.config.pipeline.modules.mapping[mapping__aligner()]
+    )
