1c550fc4a8c41fc824b22814d1a80d3bb69c9be1
Alexandre Nadin <alex.nadin12@gmail.com>
2019-02-12 16:26:44 +0100

    [pipelines] Added 'aligner' keyword to modules.

--- modules/mapping/mapping.py
+++ modules/mapping/mapping.py
@@ -1,20 +1,9 @@
-def mapping__setAligner(name):
-  workflow.globals[mapping__alignerVarName()] = name
-
 def mapping__alignerDft():
   return 'aligner'
 
-def mapping__alignerVarName():
-  return '_mapping__aligner'
-
-def mapping__getAligner():
-  if not mapping__alignerVarName() in workflow.globals.keys():
-    mapping__setAligner(mapping__alignerDft())
-  return workflow.globals[mapping__alignerVarName()]
-
 def mapping__aligner():
   try:
-    return list(pipeman.config.pipeline.modules.mapping.keys())[0]
+    return pipeman.config.pipeline.modules.mapping.aligner.name
   except:
     pipeman.log.error("No aligner found in pipeline configuration.")
 
