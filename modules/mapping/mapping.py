c70401e79dc560399874b243667e381787650ec7
Alexandre Nadin <alex.nadin12@gmail.com>
2019-02-13 14:19:49 +0100

    [modules:mapping] Included rules for sorting with picard or samtools; Manages sorted prefixes.

--- modules/mapping/mapping.py
+++ modules/mapping/mapping.py
@@ -1,3 +1,6 @@
+# ---------
+# Aligner
+# ---------
 def mapping__alignerDft():
   return 'aligner'
 
@@ -7,12 +10,15 @@ def mapping__aligner():
   except:
     pipeman.log.error("No aligner found in pipeline configuration.")
 
-def mapping__alignerDir():
+def mapping__alignerDir(append=False):
   aligner = mapping__aligner()
   if aligner:
-    return aligner
+    res = aligner
   else:
-    return mapping__alignerDft()
+    res = mapping__alignerDft()
+  if append:
+    res = os.path.sep + res
+  return res
     
 def mapping__alignerModule():
   aligner = mapping__aligner()
@@ -26,6 +32,26 @@ def mapping__includeAlignerModule():
   if aligner:
     include: mapping__alignerModule()
 
+# -----------
+# Alignment
+# -----------
+def mapping__sampleReadGroup(sample):
+  experimentName = sample.sample_run + "_" + sample.sample_name
+  platform       = pipeman.config.pipeline.sequencing.platform
+  center         = pipeman.config.pipeline.center.name 
+  return "\t".join([
+    "@RG",
+    "ID:" + experimentName, # experiment_name = sample[0]_sample[2]
+    "PL:" + platform, # PLATFORM
+    "PU:" + sample.sample_run, # sample[0]
+    "LB:" + experimentName, # experiment_name
+    "SM:" + sample.sample_name, # sample[2]
+    "CN:" + center  # CENTER
+  ])
+
+# ---------------
+# Mapping Genome 
+# ---------------
 @cluster__prefixMountPoint
 def mapping__genomeIndex():
   """
@@ -42,3 +68,28 @@ def mapping__genomeIndex():
       genome   = pipeman.config.project.genome,
       aligner  = pipeman.config.pipeline.modules.mapping[mapping__aligner()]
     )
+
+# ---------
+# Merging
+# ---------
+def picardMergeInputString(inputs=[]):
+  return " ".join([ "I={}".format(i) for i in inputs])
+
+# ---------------
+# Sorting tools
+# ---------------
+def mapping__sorter():
+  try:
+    res = pipeman.config.pipeline.modules.mapping.sorter.name
+  except:
+    res = None
+  return res
+ 
+def mapping__sorterDir(append=False):
+  sorter = mapping__sorter()
+  res = ""
+  if sorter:
+    res = os.path.sep.join(["sorted", sorter])
+    if append:
+      res = os.path.sep + res
+  return res
