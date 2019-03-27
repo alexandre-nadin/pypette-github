01bd3e95118673eb5c2da356c6f57e0f3b65bbd6
Alexandre Nadin <alex.nadin12@gmail.com>
2019-03-27 12:05:32 +0100

    [bin] Updated default variables for setting up project.

--- bin/src-env-local.sh
+++ bin/src-env-local.sh
@@ -3,12 +3,12 @@ _dir=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
 source "${_dir}/ctgb-pipe.sh"
 
-"${_dir}/set-dirs"
-
 ## Include binaries
 export PATH="${PATH}":"${_dir}"
 export CTGB__DIR_PROJECTS="${HOME}/dev/ctgb/project-analysis"
 export CLUSTER_MNT_POINT="${HOME}/dev/ctgb/cluster"
 export TMPDIR='/tmp'
+"${_dir}/set-dirs"
+
 
 condactivate ctgb-pipe
