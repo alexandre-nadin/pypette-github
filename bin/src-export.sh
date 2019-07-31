847ef487b3e0489a2d8f3b43acdb17d966d38472
Alexandre Nadin <alexandre.nadin.fr@gmail.com>
2019-07-31 15:10:46 +0200

    [bin:src-export] Fixed circular sourcing issue.

--- bin/src-export.sh
+++ bin/src-export.sh
@@ -4,9 +4,6 @@ export PATH="${PATH}:${_dir}"
 
 # Temporary way of activating conda environments
 if [ ${#_CPIPE_PIPE_ENV} -gt 0 ]; then
-  printf "Sourcing SHELL_ENV '$SHELL_ENV'\n" >&2
-  source $SHELL_ENV
-  type -a condactivate
   printf "Activating conda env '$_CPIPE_PIPE_ENV'.\n" >&2
   condeactivate || true
   condactivate $_CPIPE_PIPE_ENV
