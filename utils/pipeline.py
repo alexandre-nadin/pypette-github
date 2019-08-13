75345c5c80af7dbe5636bce171c5fc1b93d6bd80
Alexandre Nadin <alexandre.nadin.fr@gmail.com>
2019-08-13 15:25:21 +0200

    [utils:pipeline] Moved the lshell function to shell library.

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -385,15 +385,3 @@ class Pipeline():
   def __init__(self, path):
     self.path = path
     self.snakefile = None
-
-# ------
-# Shell
-# ------
-def lshell(command, allow_empty_lines=False):
-  """
-  Returns the output of a given shell command in an array.
-  Each element is an output line.
-  Filters empty strings by default.
-  """
-  out = subprocess.check_output(command, shell=True).decode().split(os.linesep)
-  return out if allow_empty_lines else [ _elem for _elem in out if _elem ] 
