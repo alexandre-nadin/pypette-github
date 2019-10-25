0e7465034cd659148236519608daa2867b02578b
Alexandre Nadin <alexandre.nadin.fr@gmail.com>
2019-10-25 17:22:59 +0200

    [modules:genomes] Wrotes rules and functions for automatically creating bed/gtf files and STAR indexes.

--- modules/genomes/genome.py
+++ modules/genomes/genome.py
@@ -1,4 +1,4 @@
-def genome__formatSpecies(func):
+def genome__formatSpeciesCfg(func):
   def wrapper(*args, **kwargs):
     try:
       species = project__speciesGenome(pipeman.config.project)
@@ -7,19 +7,41 @@ def genome__formatSpecies(func):
     return func(*args, **kwargs).format(species=species)
   return wrapper
 
-@genome__formatSpecies
+@genome__formatSpeciesCfg
 @cluster__prefixMountPoint
 def genome__dir():
   return os.path.join(
     pipeman.config.cluster.genomeDir,
     "{species.genome.assembly.ucscRef}")
 
-@genome__formatSpecies   
-def genome__fasta():
-  """
-  Retrieves the genome fastq using cluster and project metadata parameters.
-  """
+# -------------
+# Fasta Files
+# -------------
+@genome__formatSpeciesCfg   
+def genome__fastaBase():
   return os.path.join(
     genome__dir(),
     "fa",
-    "{species.genome.assembly.ucscRef}.fa")
+    "{species.genome.assembly.ucscRef}")
+
+def genome__fasta():
+  """ Produces the species' genome fasta. """
+  return f"{genome__fastaBase()}.fa"
+
+# -----------
+# 2bit Files
+# -----------
+@genome__formatSpeciesCfg   
+def genome__2bitBase():
+  return os.path.join(
+    genome__dir(), 
+    "2bit", 
+    "{species.genome.assembly.ucscRef}")
+
+def genome__2bit():
+  """ Produces the species' genome 2bit. """
+  return f"{genome__2bitBase()}.2bit"
+
+@genome__formatSpeciesCfg
+def genome__ucsc2bitUrl():
+  return pipeman.config.databases.ucsc.tbitUrl
