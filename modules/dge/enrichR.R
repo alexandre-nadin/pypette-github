9a69b0418d91d0f11a87506025bbd3e9084a7117
astascini <astascini@login1.nfs>
2019-12-02 15:19:26 +0100

    PD, removed prints, added comments on script steps

--- modules/dge/enrichR.R
+++ modules/dge/enrichR.R
@@ -6,7 +6,10 @@ suppressMessages(library("openxlsx"))
 
 databases <- listEnrichrDbs()
 
-# Parameter of the pipeline that defines the databases to make the enrichment of
+# -------------------------
+# enrichment Parameters
+# -------------------------
+# databases to make the enrichment of
 enrich.databases <- c("GO_Biological_Process_2018",
                       "GO_Cellular_Component_2018",
                       "GO_Molecular_Function_2018",
@@ -15,22 +18,17 @@ enrich.databases <- c("GO_Biological_Process_2018",
                       "WikiPathways_2016",
                       "BioCarta_2016")
 
-padj.cutoff = 0.1 
-
-print(paste("> dgeResults: ", dgeResults))
-print(paste("> names dgeResults: ", names(dgeResults)))
+# -------------------------
+# Perform Enrichment 
+# -------------------------
 
 enrichr.list <- list()
 
-print("Enrichment")
-print(c("enrichr.list: ", enrichr.list))
 for (i in 1:length(dgeResults)){
-  print(paste("> dgeResults Nb. ", i))
   .res <- dgeResults[[i]]
   up.genes   <- row.names(.res[which(.res$log2FoldChange > 0 & .res$padj < padj.cutoff),])
   down.genes <- row.names(.res[which(.res$log2FoldChange < 0 & .res$padj < padj.cutoff),])
   both.genes <- row.names(.res[which(.res$padj < padj.cutoff),])
-  print(paste("> both genes: ", both.genes))
   enrichr.list[[i]] <- lapply(list(up.genes,down.genes,both.genes),function(x) {
     enrichR::enrichr(genes = x, databases = enrich.databases)
       })  
@@ -38,14 +36,10 @@ for (i in 1:length(dgeResults)){
 }
 names(enrichr.list) <- names(dgeResults)
 
-print(paste("> names enrichr: ", names(enrichr.list)))
-#names(enrichr.list$condition_treated_vs_ctrl) = c("up","down","both")
-names(enrichr.list[[1]]) = c("up","down","both")
-print(paste("> enrichr: ", enrichr.list))
+# -----------------------------
+# Write excels files
+# -----------------------------
 
-# Writes an 3 excels for each contrasts, "up", "down", "both" with the enrichment libraries in different excel tabs.
-
-print(c("length(dgeResults): ", length(dgeResults)))
 for (i in 1:length(dgeResults)){
   for (j in c("up","down","both")){
     filename = paste(
@@ -59,3 +53,4 @@ for (i in 1:length(dgeResults)){
     write.csv(x=enrichr.list[[names(dgeResults)[i]]][[j]], file=filename, row.names=FALSE)
   }
 }
+
