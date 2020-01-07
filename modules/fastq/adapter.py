59eda18c9e4e133eee030ea7a648f4e2f60a1ba0
Alexandre Nadin <alexandre.nadin.fr@gmail.com>
2020-01-07 17:30:19 +0100

    [modules:fastq:adapter] Wrote functions to reverse-complement adapter sequences.

--- modules/fastq/adapter.py
+++ modules/fastq/adapter.py
@@ -9,3 +9,35 @@ def fastq_adapter__toolAdapters(trimmingTool):
     pipeman.config.cluster.adapterDir,
     pipeman.config.pipeline.modules.fastq.adapters[trimmingTool]
   )
+
+fastq_adapter__complBaseDic = {
+  'a': 't',
+  't': 'a',
+  'c': 'g',
+  'g': 'c'
+}
+ 
+def fastq_adapter__complement(s):
+  """
+  Complements bases from the given string :s: 
+  """
+  return ''.join(map(lambda x: fastq_adapter__complBaseDic[x], s.lower()))
+
+def fastq_adapter__reverse(s):
+  """
+  Reverses the given :s: string
+  """
+  return ''.join(reversed(s)).strip()
+
+def fastq_adapter__reverseComplementFasta(f):
+  """
+  Reads an input fasta file and reverse-complements each adapter.
+  Tags headers starting with '>'.
+  """
+  with open(f, 'r') as fh:
+    return (
+      fastq_adapter__complement(fastq_adapter__reverse(line)).upper()
+      if not line.startswith('>')
+      else line + "_reversed"
+      for line in map(str.strip, fh.readlines()))
+
