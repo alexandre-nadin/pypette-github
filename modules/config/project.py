11366a8c1491c88972ff988a7baba6fa51ec0e11
Alexandre Nadin <alexandre.nadin.fr@gmail.com>
2020-01-16 15:05:03 +0100

    [modules:qc] Set project outdir inside the sequencing run.

--- modules/config/project.py
+++ modules/config/project.py
@@ -1,31 +1,5 @@
 project__samplesTarget = "samples/samples.csv"
 
-def project__samplesTargetPath():
-  return os.path.join(
-    project__projectsDir(),
-    "{project}",
-    project__samplesTarget
-  )
-
-def project__projectsDir():
-  return os.path.join(
-    pypette.config.cluster.stdAnalysisDir,
-    pypette.config.cluster.projects.outDir)
-
-def project__dirFmt():
-  return os.path.join(
-    project__projectsDir(), 
-    "{project}")
-
-def project__dir(project):
-  return project__dirFmt().format(project=project)
-
-def project__samplesMetaPath(project):
-  """ Returns a project samples file's absolute path """
-  return os.path.join(
-    project__dir(project), 
-    project__samplesTarget)
-
 def project__pipelineQcTarget(pipeline, formatted=False, **kwargs):
   """ Includes the correct QC target for the given pipeline's module. """
   pypette.includeModule(f"qc/{pipeline}.py")
