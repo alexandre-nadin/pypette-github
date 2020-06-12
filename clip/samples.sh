b73473bba2afc5dd073821ccad91dfcbe9b8320f
Alexandre Nadin <alex.nadin12@gmail.com>
2020-06-12 17:16:46 +0200

    [clip:samples] Added functions for temporary genome files.

--- clip/samples.sh
+++ clip/samples.sh
@@ -94,3 +94,50 @@ samples-are-all-selected ()
 {
   [ $(samples-selected | wc -l) -eq $(samples | wc -l) ]
 }
+
+# ------------
+# Temporaries
+# ------------
+SPLS_TMP_EXTS='gz\|bam'
+sample-tmps ()
+#
+# Lists all temporaries for the samples given in stdin
+#
+{
+  for spl in $(cat /dev/stdin); do
+    find samples/${spl}/runs/$(clip-run) -type f -regex ".*\.\(${SPLS_TMP_EXTS}\)"
+  done
+}
+
+samples-tmps ()
+#
+# Lists all the temporaries for all samples.
+#
+{
+  samples | sample-tmps
+}
+
+sample-rm-tmps ()
+#
+# Removes all temporaries for samples given in STDIN.
+#
+{
+  cat /dev/stdin | sample-tmps | xargs rm
+}
+
+GNMS_TMP_EXTS='gz\|bed\|gtf\|fa\|fasta'
+genomes-tmps()
+#
+# Lists all temporaries for genomes.
+#
+{
+  find genomes -type f -regex ".*\(${GNMS_TMP_EXTS}\)"
+}
+
+genomes-rm-temps()
+#
+# Removes all temporaries for genomes.
+#
+{
+  genomes-tmps | xargs rm
+}
